99c375c8399dd9aec2949e813b2339903bd139aa
Byron Tasseff <byron@tasseff.com>
2022-05-11 10:04:54 -0600

    Update parallel tempering interface for more accurate timing statistics. Add option for using geometric or custom initial beta distribution.

--- pt_par.py
+++ pt_par.py
@@ -19,10 +19,11 @@ from collections import namedtuple
 
 import bqpjson
 
-def write_bqpjson_as_txt(directory, data):
-    output_path = os.path.join(directory, "input.txt")
 
-    with open(output_path, "w") as output:
+def write_bqpjson_as_txt(directory, data):
+    output_path = os.path.join(directory, 'input.txt')
+
+    with open(output_path, 'w') as output:
         for lt in data['linear_terms']:
             output.write('1\t{:d}\t{:.6f}\n'.format(lt['id'], lt['coeff']))
     
@@ -34,14 +35,16 @@ def write_bqpjson_as_txt(directory, data):
 
 
 def parse_result(result_path):
-    with open(result_path, "r") as output:
-        energy_lines = output.readlines()[1:-1]
-        energies = [float(x.split('\t')[1]) for x in energy_lines]
+    with open(result_path, 'r') as output:
+        energy_lines = output.readlines()[1:]
+        energies = [float(x.split('\t')[2]) for x in energy_lines]
+        solve_times = [float(x.split('\t')[1]) for x in energy_lines]
         minimum_energy_index = np.argmin(energies, axis = 0)
         minimum_energy = energies[minimum_energy_index]
+        solve_time = solve_times[minimum_energy_index]
         best_solution = energy_lines[minimum_energy_index].split(' ')[-1]
 
-    return minimum_energy, best_solution
+    return minimum_energy, best_solution, solve_time
 
 
 def main(args):
@@ -61,16 +64,25 @@ def main(args):
     input_path = write_bqpjson_as_txt(tmp_directory, data)
     input_name = os.path.splitext(os.path.basename(input_path))[0]
 
+    # Get the directory of the SVMC executable.
+    pt_dir = os.path.dirname(shutil.which('parallel_tempering'))
+    beta_distribution_path = os.path.join(pt_dir, 'BetaDistribution8.txt')
+    new_beta_distribution_path = os.path.join(tmp_directory, 'InitialBetaDistribution.txt')
+    shutil.copy(beta_distribution_path, new_beta_distribution_path)
+
+    # Determine if geometric or custom beta distribution should be used.
+    geometric_beta = 1 if not args.use_geometric else 0
+
     start_time = time.time()
-    subprocess.call(["mpiexec", "-np", "8", "parallel-tempering", "64", \
-        "2", "0.1", "5.0", "0", "0", "1", str(args.runtime_limit), "8", \
-        "-999999.9", "0", input_name], stdout = subprocess.DEVNULL, cwd = tmp_directory)
+    subprocess.call(['mpiexec', '-np', '8', 'parallel_tempering', '64', \
+        '2', '0.1', '5.0', '0', str(geometric_beta), '1', str(args.runtime_limit), '8', \
+        '-999999.9', '0', input_name], stdout = subprocess.DEVNULL, cwd = tmp_directory)
     elapsed_time = time.time() - start_time
 
-    result_path = os.path.join(tmp_directory, "EnergiesFound_input.dat")
+    result_path = os.path.join(tmp_directory, 'EnergiesFound_input.dat')
 
     # Get the best energy, corresponding solution, and metadata.
-    energy, solution = parse_result(result_path)
+    energy, solution, solve_time = parse_result(result_path)
     nodes = len(data['variable_ids'])
     edges = len(data['quadratic_terms'])
 
@@ -84,12 +96,12 @@ def main(args):
     print()
 
     if args.show_solution:
-        bqp_solution = ', '.join(["-1" if solution[i] == "0" else "1" for i in range(0, nodes)])
+        bqp_solution = ', '.join(['-1' if solution[i] == '0' else '1' for i in range(0, nodes)])
         print('BQP_SOLUTION, %d, %d, %f, %f, %s' % \
-            (nodes, edges, energy, elapsed_time, bqp_solution))
+            (nodes, edges, energy, solve_time, bqp_solution))
 
     print('BQP_DATA, %d, %d, %f, %f, %f, %f, %f, %d, %d' % \
-        (nodes, edges, energy, scaled_lower_bound, energy, lower_bound, elapsed_time, 0, 0))
+        (nodes, edges, energy, scaled_lower_bound, energy, lower_bound, solve_time, 0, 0))
 
     # Clean up.
     shutil.rmtree(tmp_directory)
@@ -99,6 +111,7 @@ def build_cli_parser():
     parser.add_argument('-f', '--input-file', help='the data file to operate on (.json)')
     parser.add_argument('-ss', '--show-solution', help='prints the a solution data line', action='store_true', default=False)
     parser.add_argument('-rtl', '--runtime-limit', help='runtime limit (iters.)', type=int)
+    parser.add_argument('-g', '--use-geometric', help='whether geometric or custom beta dist. is used', action='store_true', default=False)
     return parser
 
 
